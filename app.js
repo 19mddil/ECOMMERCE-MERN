649ffdcc9196c2b3fd6221feb56b1bfafd567a39
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-01-04 15:04:34 +0600

    authorization and promise rejection handle

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('express-async-errors');
+const error = require("./middlewares/errors");
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,8 +8,10 @@ const userRouter = require('./routers/userRouter');
 
 app.use(express.json());
 app.use(cors());
+
 app.get('/', (req, res) => res.send("hello"));
 app.use('/api/user', userRouter);
+app.use(error);
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -9,41 +9,32 @@ module.exports.SignUp = async (req, res) => {
     let { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     let user = {};
-    try {
-        if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(process.env.MONGODB_URL_LOCAL);
-            mongoose.set('strictQuery', true);
-            console.log("Connection successfull");
-        }
-        user = await User.findOne({ email: req.body.email });
-        if (user) return res.status(400).send('User Already Exists');
-        const salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(req.body.password, salt);
-        user = await User.create(req.body);
-        res.status(201).send({ messeage: "Registraton Successful", user: _.pick(user, ['name', 'email', 'password', 'role']), token: user.genJWT() });
-
-    } catch (error) {
-        res.send(500).send("Server Error");
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(process.env.MONGODB_URL_LOCAL);
+        mongoose.set('strictQuery', true);
+        console.log("Connection successfull");
     }
+    user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User Already Exists');
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
+    user = await User.create(req.body);
+    res.status(201).send({ messeage: "Registraton Successful", user: _.pick(user, ['name', 'email', 'password', 'role']), token: user.genJWT() });
+
 }
 
 module.exports.SignIn = async (req, res) => {
-    try {
-        if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(process.env.MONGODB_URL_LOCAL);
-            mongoose.set('strictQuery', true);
-            console.log("Connection successfull");
-        }
-        const user = await User.findOne({ email: req.body.email });
-        if (!user) return res.status(400).send("email or password error");
-        const boolvar = await bcrypt.compare(req.body.password, user.password);
-        if (boolvar) {
-            res.status(201).send({ messeage: "Login Successful", user: _.pick(user, ['name', 'email', 'password', 'role']), token: user.genJWT() });
-        } else {
-            res.status(200).send("username or password error");
-        }
-
-    } catch (e) {
-        res.status(400).send(errors(e));
+    if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(process.env.MONGODB_URL_LOCAL);
+        mongoose.set('strictQuery', true);
+        console.log("Connection successfull");
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("email or password error");
+    const boolvar = await bcrypt.compare(req.body.password, user.password);
+    if (boolvar) {
+        res.status(201).send({ messeage: "Login Successful", user: _.pick(user, ['name', 'email', 'password', 'role']), token: user.genJWT() });
+    } else {
+        res.status(200).send("username or password error");
     }
 }
