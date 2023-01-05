406ec151f2770f531f3e636756af5560a820d33f
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-01-05 20:32:06 +0600

    catagory model and router

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const userRouter = require('./routers/userRouter');
+const categoryRouter = require('./routers/categoryRouter');
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => res.send("hello"));
 app.use('/api/user', userRouter);
+app.use('/api/category', categoryRouter);
 app.use(error);
 
 if (process.env.NODE_ENV === 'development') {
