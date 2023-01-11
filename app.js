0d76441e167e2961d6f57dc550c769fa1feb3264
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-01-12 05:44:24 +0600

    refractored some code

--- app.js
+++ app.js
@@ -2,24 +2,9 @@ require('express-async-errors');
 const error = require("./middlewares/errors");
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
-const userRouter = require('./routers/userRouter');
-const categoryRouter = require('./routers/categoryRouter');
-const productRouter = require('./routers/productRouter');
 
-app.use(express.json());
-app.use(cors());
-
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
-
-
-app.get('/', (req, res) => res.send("hello"));
-app.use('/api/user', userRouter);
-app.use('/api/category', categoryRouter);
-app.use('/api/product', productRouter);
+require('./middlewares')(app);
+require('./middlewares/routes')(app);
 
 app.use(error);
 
---
+++ middlewares/routes.js
@@ -0,0 +1,10 @@
+const userRouter = require('../routers/userRouter');
+const categoryRouter = require('../routers/categoryRouter');
+const productRouter = require('../routers/productRouter');
+
+module.exports = (app) => {
+    app.get('/', (req, res) => res.send("hello"));
+    app.use('/api/user', userRouter);
+    app.use('/api/category', categoryRouter);
+    app.use('/api/product', productRouter);
+}
