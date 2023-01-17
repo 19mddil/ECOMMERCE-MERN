7f94a1408d7677d531172cc3adede77d8e197540
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-01-17 18:57:14 +0600

    added some frontend react libraries

---
+++ ecom-frontend/src/components/Main.js
@@ -0,0 +1,12 @@
+import { Routes, Route, Navigate } from "react-router";
+const Main = () => {
+    return (
+        <div>
+            <Routes>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default Main;
