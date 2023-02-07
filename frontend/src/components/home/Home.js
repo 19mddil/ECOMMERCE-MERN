95dbbb294e953ea99a1d7d053ef64afb867c3eb1
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-02-07 15:51:05 +0600

    loaded all products in cards in homepage

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,12 +1,36 @@
-
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../Layout';
 import { isAuthenticated } from '../../utils/auth'
+import { getCategories, getProducts, getProductDetails } from '../../api/apiProduct';
+import { showError, showSuccess } from '../../utils/messages'
+import Card from './Card';
+
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [limit, setLimit] = useState(30);
+    const [order, setOrder] = useState('asc');
+    const [sortBy, setSortBy] = useState('price');
+
+    useEffect(() => {
+        getProducts(sortBy, order, limit)
+            .then(res => setProducts(res.data))
+            .catch(err => setError("Failed to load products"))
+    }, []);
+
+
     return (
         <div>
             {isAuthenticated() && <Layout title='Home Page' className='container' >
-                I am homepage.
+                <div style={{ width: '100%' }}>
+                    {showError(error, error)}
+                    {showSuccess(success, "added to cart")}
+                </div>
+                <div className='row'>
+                    {products && products.map(product => <Card product={product} key={product._id} />)}
+                </div>
             </Layout>}
             {!isAuthenticated() && <Layout title='Home Page' className='container' >
                 jwt token expried
